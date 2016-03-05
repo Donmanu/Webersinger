4dbfb4a10fc7148bc1ab417b28d29920a9658a06
Donmanu <manuel.giesinger92@gmail.com>
2016-03-05 20:34:24 +0100

    Enemy_Bullet deleted

--- core/src/com/don/galaxydefender/logic/ProjectileType.java
+++ core/src/com/don/galaxydefender/logic/ProjectileType.java
@@ -2,6 +2,6 @@ package com.don.galaxydefender.logic;
 
 
 public enum ProjectileType {
-    BULLET, ENEMY_BULLET, LASER, FLAME, PLASMA, ROCKET, BOMB, ENERGY,
+    BULLET, LASER, FLAME, PLASMA, ROCKET, BOMB, ENERGY,
     IMPLOSION, DISINTEGRATION, EMP, ATOMIC, PARTICLE
 }
