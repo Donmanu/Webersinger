f7ad84b9b9da8f70dce8bd9f89918e2bf2f69067
Carsten <webercarsten9@aol.com>
2016-07-17 12:43:58 +0200

    Testing

--- core/src/com/don/galaxydefender/logic/Enemy.java
+++ core/src/com/don/galaxydefender/logic/Enemy.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.math.Rectangle;
 
 public class Enemy extends Living {
+    //Test Konfig
 
     public Enemy(EnemyType enemyType) {
         switch (enemyType){
